9c2e92434b7943def8bbfb846ad188370a2631bc
Raylen Margono <raylenmargono@gmail.com>
2016-06-05 21:43:43 -0700

    submission

--- Vestivise/settings.py
+++ Vestivise/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -85,7 +86,7 @@ DATABASES = {
     }
 }
 
-import dj_database_url
+
 DATABASES['default'] = dj_database_url.config()
 
 
