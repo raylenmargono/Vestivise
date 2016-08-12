82bfa675a5177439789202ce2a400aca39f4801b
alexnovak <xeloras@gmail.com>
2016-08-12 01:41:48 -0400

    Basic error handling on data algos

--- data/algos.py
+++ data/algos.py
@@ -22,32 +22,43 @@ def basicRisk(request):
 	NOTE: Change pandas.io.data to use pandas_datareader
 	in the future, to prevent complaining.
 	'''
+	try:
+		accounts = request.user.profile.data.yodleeAccounts.all()
+		if(not accounts):
+			return JsonResponse({})
+		#Hideous, bug constructs a list of each account's percentage of the
+		#overall account.
+		acctWeights = np.array([sum([x.value.amount for x in a.holdings.filter(createdAt__exact = a.updatedAt)]) for a in accounts])
+		if(acctWeights == [0]):
+			return JsonResponse({})
+		acctWeights = acctWeights/sum(acctWeights)
+		#Also hideous, but constructs a list of touples with the symbols
+		#and their corresponding (now correct) weights
+		allocations = [(h.symbol, h.assetClassifications.all()[0].allocation*w/100) for h in a.holdings.filter(createdAt__exact = a.updatedAt) for a,w in zip(accounts, acctWeights)]
 
-	accounts = request.user.profile.data.yodleeAccounts.all()
-	#Hideous, bug constructs a list of each account's percentage of the
-	#overall account.
-	acctWeights = np.array([sum([x.value.amount for x in a.holdings.all()]) for a in accounts])
-	acctWeights = acctWeights/sum(acctWeights)
-	#Also hideous, but constructs a list of touples with the symbols
-	#and their corresponding (now correct) weights
-	allocations = [(h.symbol, h.assetClassifications.all()[0].allocation*w/100) for h in a.holdings.all() for a,w in zip(accounts, acctWeights)]
+		#With the hideous part out of the way, pandas makes everything else
+		#easy. 
 
-	#With the hideous part out of the way, pandas makes everything else
-	#easy. 
+		symbols = [h[0] for h in allocations]
+		weight = np.matrix([h[1] for h in allocations])
 
-	symbols = [h[0] for h in allocations]
-	weight = np.matrix([h[1] for h in allocations])
+		secRets = web.DataReader(symbols, 'yahoo', datetime.datetime.now()-datetime.timedelta(years=1), datetime.datetime.now-datetime.timedelta(days=1))['Close'].pct_change()
 
-	secRets = web.DataReader(symbols, 'yahoo', datetime.datetime.now()-datetime.timedelta(years=1), datetime.datetime.now-datetime.timedelta(days=1))['Close'].pct_change()
+		mu = np.matrix(secRets.mean().as_matrix())
+		sigma = np.matrix(secRets.cov())
+		#Collect the current risk free rate of return
+		rfrr = web.DataReader('^IRX', 'yahoo', datetime.datetime.now()-datetime.timedelta(days=1))['Close'][0]
 
-	mu = np.matrix(secRets.mean().as_matrix())
-	sigma = np.matrix(secRets.cov())
-	#Collect the current risk free rate of return
-	rfrr = web.DataReader('^IRX', 'yahoo', datetime.datetime.now()-datetime.timedelta(days=1))['Close'][0]
+		ratio = (weight.T*mu - rfrr) / np.sqrt(weight.T * sigma * weight)
 
-	ratio = (weight.T*mu - rfrr) / np.sqrt(weight.T * sigma * weight)
+		return JsonResponse({'ratio':ratio.A[0][0]}, status=200)
+	except OSError as err:
+		print(err)
+		return JsonResponse({})
+	else:
+		print(err)
+		return JsonResponse({})
 
-	return JsonResponse({'ratio':ratio.A[0][0]}, status=200)
 
 def basicCost(request):
 	'''
@@ -60,10 +71,16 @@ def basicCost(request):
 	ratio. 
 	{'ERsum' : <value>}
 	'''
-	accounts = request.user.profile.data.yodleeAccounts.all()
-	invOptions = itertools.chain([x.investmentOptions.all() for x in accounts])
-	ers = [x.netExpenseRatio for x in invOptions]
-	return JsonResponse({'ERsum': np.sum(ers)}, status=200)
+	try:
+		accounts = request.user.profile.data.yodleeAccounts.all()
+		if(not accounts):
+			return JsonResponse({})
+		invOptions = itertools.chain([x.investmentOptions.all() for x in accounts])
+		ers = [x.netExpenseRatio for x in invOptions]
+		return JsonResponse({'ERsum': np.sum(ers)}, status=200)
+	except:
+		print(err)
+		return JsonResponse({})
 
 def basicReturns(request):
 	'''
@@ -77,10 +94,16 @@ def basicReturns(request):
 	investment option, keys are the symbol of that
 	option. 
 	'''
-	accounts = request.user.profile.data.yodleeAccounts.all()
-	invOptions = itertools.chain([x.investmentOptions.all() for x in accounts])
-	returnDict = dict([(x.symbol, x.historicReturns) for x in invOptions])
-	return JsonResponse(returnDict, status=200)
+	try:
+		accounts = request.user.profile.data.yodleeAccounts.all()
+		if(not accounts):
+			return JsonResponse({})
+		invOptions = itertools.chain([x.investmentOptions.all() for x in accounts])
+		returnDict = dict([(x.symbol, x.historicReturns) for x in invOptions])
+		return JsonResponse(returnDict, status=200)
+	except:
+		print(err)
+		return JsonResponse({})
 
 def basicAssets(request):
 	'''
@@ -95,18 +118,26 @@ def basicAssets(request):
 	and 'totalInvested' to the total amount invested in
 	the portfolio.
 	'''
-	accounts = request.user.profile.data.yodleeAccounts.all()
-	holds = itertools.chain([x.holdings.get(createdAt=x.lastHoldingsUpdate) for x in accounts])
-	holdingValues = {}
-	totalValue = 0
-	for h in holds:
-		if h.holdingType in holdingValues:
-			holdingValues[h.holdingType] += h.value.amount
-		else:
-			holdingValues[h.holdingType] = h.value.amount
-		totalValue += h.value.amount
-	for h in holdingValues:
-		holdingValues[h] = holdingValues[h]/totalValue
-	return JsonResponse({'percentages': holdingValues,
-						'totalInvested':totalValue},
-						status=200)
+	try:
+		accounts = request.user.profile.data.yodleeAccounts.all()
+		if(not accounts):
+			return JsonResponse({})
+		holds = itertools.chain([x.holdings.filter(createdAt__exact = a.updatedAt) for x in accounts])
+		if(not accounts):
+			return JsonResponse({})
+		holdingValues = {}
+		totalValue = 0
+		for h in holds:
+			if h.holdingType in holdingValues:
+				holdingValues[h.holdingType] += h.value.amount
+			else:
+				holdingValues[h.holdingType] = h.value.amount
+			totalValue += h.value.amount
+		for h in holdingValues:
+			holdingValues[h] = holdingValues[h]/totalValue
+		return JsonResponse({'percentages': holdingValues,
+							'totalInvested':totalValue},
+							status=200)
+	except:
+		print(err)
+		return JsonResponse({})		
