89b2cc49fc33953ed91651beb6a844676282c297
Raylen Margono <raylenmargono@gmail.com>
2016-08-25 19:58:46 +0700

    debug true

--- Vestivise/settings.py
+++ Vestivise/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = secret_key
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['app.vestivise.com', 'localhost', '127.0.0.1']
 
 
