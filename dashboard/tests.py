1e4d161e1cda9c8456b1d4a6943ff4d53658e8ea
Raylen Margono <raylenmargono@gmail.com>
2016-10-08 13:03:44 -0400

    quote test

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+class QuovoTest(TestCase):
+
+    def test_token_is_valid(self):
+        false_response_date = "2016-03-31T18:45:09Z"
+        correct_response_date = "2216-03-31T18:45:09Z"
+        
