384b172da815be8ef7eaf2df7ca06064955b8f73
Raylen Margono <raylenmargono@gmail.com>
2016-12-25 00:50:20 -0800

    install git

--- webhooks/githook.py
+++ webhooks/githook.py
@@ -4,13 +4,13 @@ import os
 from rest_framework.decorators import permission_classes
 from Vestivise import permission
 from Vestivise.settings import BASE_DIR
-
-
+import git
 
 @permission_classes((permission.GitHubWebHookPermission, ))
 @csrf_exempt
 def git_post_receive(request):
     path = os.path.join(BASE_DIR, 'runScripts/')
-    os.system("git pull")
+    g = git.cmd.Git(BASE_DIR)
+    g.pull()
     os.system(path + 'vestivise_startup.sh')
     return HttpResponse("Success")
