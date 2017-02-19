5d7ff77a874d701969c1999b358770fadea1050c
Raylen Margono <raylenmargono@gmail.com>
2017-02-19 18:07:47 -0500

    change nav bar on demo

--- staticfiles/src/jsx/apps/clientDashboard/dashboard/floatingNav.jsx
+++ staticfiles/src/jsx/apps/clientDashboard/dashboard/floatingNav.jsx
@@ -25,7 +25,7 @@ class FloatingNav extends Component{
         if(this.props.isDemo){
             return(
                 <div id="navigation">
-                    <a href={Urls.signUpPage()} >Contact</a>
+                    <a href={Urls.signUpPage()} >Free Sign Up</a>
                 </div>
             );
         }
